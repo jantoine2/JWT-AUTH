2d6fbc85bcd2224a80ec885bcbbd7367b3ccce0b
Vico509 <antoinejovany99@gmail.com>
2024-04-05 14:05:10 -0400

    The passport-jwt options object(with jsonwebtoken options)

--- config/passport.js
+++ config/passport.js
@@ -2,9 +2,13 @@ const fs = require('fs');
 const passport = require('passport');
 const path = require('path');
 const User = require('mongoose').model('User');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
 
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
+
+
 
 // TODO
 const options = {};
