2816d46943e6b346c7864d996eaca1566cb47d92
Vico509 <antoinejovany99@gmail.com>
2024-04-05 14:40:36 -0400

    Implementing the register route

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,23 @@ router.get('protected', (req, res, next) => {
 router.post('/login', function(req, res, next) {});
 
 // TODO
-router.post('/register', function(req, res, next){});
+router.post('/register', function(req, res, next){
+    const saltHash = utils.genPassword(req.body.password);
+
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+
+    const newUser = new User({
+        username: req.body.username,
+        hash: hash,
+        salt: salt
+    });
+
+    newUser.save()
+        .then((user) => {
+            res.json({ success: true, user: user });
+        })
+        .catch(err => next(err));
+});
 
 module.exports = router;
