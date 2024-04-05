ba6a2b32489bbc3bf4b299e0da95ffc2321af36c
Vico509 <antoinejovany99@gmail.com>
2024-04-05 00:42:37 -0400

    finish setup

---
+++ config/passport.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const passport = require('passport');
+const path = require('path');
+const User = require('mongoose').model('User');
+
+const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
+const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
+
+// TODO
+const options = {};
+
+// TODO
+module.exports = (passport) => {}
