1f592e2ddeda23206fe86d59ad18961fd57f1236
Vico509 <antoinejovany99@gmail.com>
2024-04-05 14:29:01 -0400

    The passport-jwt verify callback function configuration

--- config/passport.js
+++ config/passport.js
@@ -8,14 +8,28 @@ const ExtractJWT = require('passport-jwt').ExtractJwt;
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
-
-
-// TODO
 const options = {
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: PUB_KEY,
     algorithm: ['RS256']
 };
 
-// TODO
-module.exports = (passport) => {}
+const strategy = new JWTStrategy(options, (payload, done) => {
+
+    User.findOne({ _id: payload.sub })
+        .then((user) => {
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+
+        })
+        .catch(err => done(err, null));
+
+});
+
+
+module.exports = (passport) => {
+    passport.use(strategy);
+}
